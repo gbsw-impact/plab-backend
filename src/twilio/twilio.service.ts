78f7dd6fb9c81fb1e8c34343790c4d638ad4b2e8
Suhwan Lee <acmquwjm@gmail.com>
2024-06-21 02:11:20 +0900

    임시 변경 사항 저장

--- src/twilio/twilio.service.ts
+++ src/twilio/twilio.service.ts
@@ -4,13 +4,14 @@ import { Twilio } from 'twilio';
 @Injectable()
 export class TwilioService {
   private readonly client: Twilio;
-  private readonly twilioPhoneNumber: string = '+15189636426';
+  private readonly twilioPhoneNumber: string;
 
   constructor() {
     this.client = new Twilio(
-      'ACd1fcb53115ff003da47da1aabe6fd0aa',
-      '543505e024dc22b2dd4ea3e489bfc13c',
+      process.env.TWILIO_ACCOUNT_SID,
+      process.env.TWILIO_AUTH_TOKEN,
     );
+    this.twilioPhoneNumber = process.env.TWILIO_PHONE_NUMBER;
   }
 
   async sendSms(to: string, body: string) {
