76922aa209b9a54ce433fcde3c59cf7b885d1691
Suhwan Lee <acmquwjm@gmail.com>
2024-06-25 12:28:33 +0900

    config.json 수정

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,6 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import * as path from 'path';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
-import { UserModule } from './boards/user.module';
 import { ArticleModule } from './article/article.module';
 import { APP_FILTER } from '@nestjs/core';
 import { HttpExceptionFilter } from './filter/http-exception.filter';
@@ -11,6 +10,7 @@ import { UserAuthority } from './entities/user-authority.entity';
 import { LabModule } from './lab/lab.module';
 import { adminModule } from './admin/admin.module';
 import { TwilioModule } from './twilio/twilio.module';
+import { UserModule } from './boards/user.module';
 
 @Module({
   imports: [
